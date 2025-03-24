70e71c2ba4ffcd18f01ffb7027c80b21c1f146c5
JManRealNig <jmanrealnig@gmail.com>
2025-03-24 07:37:43 +0000

    Updated project from new Bolt account

--- src/components/SplineAnimation.tsx
+++ src/components/SplineAnimation.tsx
@@ -12,7 +12,7 @@ function SplineAnimation() {
   return (
     <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full pointer-events-none z-0">
       <spline-viewer 
-        url="https://prod.spline.design/tTaM149XhMNGxxfZ/scene.splinecode"
+        url="https://prod.spline.design/Dta7Wufq-v3-SWID/scene.splinecode"
         className="w-full h-full"
       />
     </div>
